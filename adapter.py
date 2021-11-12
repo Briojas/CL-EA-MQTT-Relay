c860b4f8ac4958caedb5a8058023efef31f48abc
Briojas <blake.a.riojas@gmail.com>
2021-11-12 14:39:45 -0600

    added bridge builder

--- adapter.py
+++ adapter.py
@@ -2,21 +2,24 @@ from bridge import Bridge
 
 
 class Adapter:
-    brokers = [{
-        'Host': 'mqtt.eclipse.org',
-        'Port': 1883,
-        'User': eclipseUserName,
-        'API': ECLIPSE_API_KEY
+    bridges = [{
+        'bridge': None,
+        'host': 'mqtt.eclipse.org',
+        'port': 1883,
+        'user': eclipseUserName,
+        'key': ECLIPSE_API_KEY
     },{
-        'Host': 'test.mosquitto.org',
-        'Port': 1883,
-        'User': eclipseUserName,
-        'API': ECLIPSE_API_KEY
+        'bridge': None,
+        'host': 'test.mosquitto.org',
+        'port': 1883,
+        'user': eclipseUserName,
+        'key': ECLIPSE_API_KEY
     },{
-        'Host': 'broker.hivemq.com',
-        'Port': 1883,
-        'User': eclipseUserName,
-        'API': ECLIPSE_API_KEY
+        'bridge': None,
+        'host': 'broker.hivemq.com',
+        'port': 1883,
+        'user': eclipseUserName,
+        'key': ECLIPSE_API_KEY
     }]
     action_list = ['subscribe', 'publish']
     action = ''
@@ -35,9 +38,11 @@ class Adapter:
         self.id = input.get('id', 'NONE')
         self.request_data = input.get('data')
         if self.validate_request_data():
-            self.id_action()
-            self.create_request()
-            self.bridge = Bridge()
+            if self.id_action():
+                self.build_bridges()
+                self.create_request()
+            else:
+                self.result_error('Invalid action provided')
         else:
             self.result_error('No data provided')
 
@@ -48,11 +53,21 @@ class Adapter:
             return False
         return True
 
+    def build_bridges(self):
+        for bridge in self.bridges:
+            bridge['bridge'] = Bridge(
+                bridge['host'],
+                bridge['port'],
+                bridge['user'],
+                bridge['key']
+            )
+
     def id_action(self):
         for action in self.actions:
             if self.request_data[action] is not None:
                 self.action = action
-                break
+                return True
+        return False
 
     def create_request(self):
         try:
